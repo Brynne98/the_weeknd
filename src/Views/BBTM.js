5d80c9b3de1fcb468ebce3521355bbf787188cd0
Brynne98 <36798170+Brynne98@users.noreply.github.com>
2020-04-30 19:52:21 +0200

    Putting my website on github pages

--- src/Views/BBTM.js
+++ src/Views/BBTM.js
@@ -7,10 +7,6 @@ class BBTM extends React.Component {
     render() {
 
         return <Fragment>
-            <div id='rg_embed_link_727466' className='rg_embed_link' data-song-id='727466'>Read <a
-                href='https://genius.com/The-weeknd-the-hills-lyrics'>“The Hills” by The Weeknd</a> on Genius
-            </div>
-            <script crossOrigin src='//genius.com/songs/727466/embed.js'></script>
             This is Beauty behind the madness
         </Fragment>
     }
